048aa72b5342d892553c977aac4a8fc2512270de
marigomez30 <mariana.gomeza@upb.edu.co>
2024-04-12 10:30:56 -0500

    Codigo explicacion de delegado y pp

---
+++ Delegado/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+delegate void ReproductorEvento(string mensaje);
+
+
+
+class Program {
+    static void ManejarInicioCancion(string mensaje) {
+        Console.WriteLine($"Mensaje recibido: {mensaje}");
+    }
+
+    static void Main(string[] args) {
+        string Cancion;
+
+        ReproductorMusica reproductor = new ReproductorMusica();
+
+        reproductor.CancionComenzada += ManejarInicioCancion;
+        Cancion = Console.ReadLine();
+        reproductor.IniciarCancion(Cancion);
+
+        
+    }
+}
+
+class ReproductorMusica
+{
+    public event ReproductorEvento CancionComenzada;
+
+    public void IniciarCancion(string nombreCancion)
+    {
+        Console.WriteLine($"Reproduciendo: {nombreCancion}");
+        if (CancionComenzada != null)
+        {
+            CancionComenzada($"Comenzó a reproducirse: {nombreCancion}");
+        }
+    }
+}
