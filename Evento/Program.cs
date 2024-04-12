2efbfe802a69e98db79526e2dcc6de0eb676d872
marigomez30 <mariana.gomeza@upb.edu.co>
2024-04-12 10:42:46 -0500

    Evento

---
+++ Evento/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+delegate void MyEventHandler(string message);
+
+class MyClass
+{
+    public event MyEventHandler MyEvent;
+
+    public void RaiseEvent(string message)
+    {
+        if (MyEvent != null)
+        {
+            MyEvent(message);
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+
+        myObject.MyEvent += MyEventHandlerMethod;
+
+        myObject.RaiseEvent("Si sale esto funciona jajaja");
+
+        Console.ReadLine();
+    }
+
+    static void MyEventHandlerMethod(string message)
+    {
+        Console.WriteLine($"Event raised: {message}");
+    }
+}
