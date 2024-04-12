c0c64ae05dbf5957bc04dfea1f977bbc4e318fa5
marigomez30 <mariana.gomeza@upb.edu.co>
2024-04-12 11:25:16 -0500

    Lista funciones

--- Planilla de Funciones/Program.cs
+++ Planilla de Funciones/Program.cs
@@ -223,3 +223,103 @@ void ShuffleArray(int[] array)
 }
 
 //Fuincion #21
+bool EsFechaValida(int dia, int mes, int anio)
+{
+    try
+    {
+        DateTime fecha = new DateTime(anio, mes, dia);
+        return true;
+    }
+    catch (Exception)
+    {
+        return false;
+    }
+}
+
+//Fuincion #22
+double CalcularVolumenEsfera(double radio)
+{
+    return (4.0 / 3.0) * Math.PI * Math.Pow(radio, 3);
+}
+
+//Fuincion #23
+string InvertirCadena(string texto)
+{
+    char[] caracteres = texto.ToCharArray();
+    Array.Reverse(caracteres);
+    return new string(caracteres);
+}
+
+//Fuincion #24
+int GenerarNumeroAleatorio(int min, int max)
+{
+    Random rand = new Random();
+    return rand.Next(min, max + 1);
+}
+
+//Fuincion #25
+int ContarVocales(string palabra)
+{
+    int contador = 0;
+    foreach (char c in palabra.ToLower())
+    {
+        if ("aeiou".Contains(c))
+            contador++;
+    }
+    return contador;
+}
+
+
+//Fuincion #26
+string ConvertirAMayusculas(string texto)
+{
+    return texto.ToUpper();
+}
+
+//Fuincion #26
+double CalcularPromedio(List<double> numeros)
+{
+    if (numeros.Count == 0)
+        return 0;
+
+    double suma = 0;
+    foreach (var num in numeros)
+    {
+        suma += num;
+    }
+    return suma / numeros.Count;
+}
+
+//Fuincion #27
+int Minimo(int a, int b)
+{
+    return a < b ? a : b;
+}
+
+//Fuincion #28
+bool EsPrimo(int numero)
+{
+    if (numero <= 1)
+        return false;
+
+    for (int i = 2; i * i <= numero; i++)
+    {
+        if (numero % i == 0)
+            return false;
+    }
+    return true;
+}
+
+//Fuincion #29
+double CalcularAreaTriangulo(double baseTriangulo, double altura)
+{
+    return (baseTriangulo * altura) / 2;
+}
+
+//Fuincion #30
+
+bool EsPalindromoSinEspacios(string palabra)
+{
+    palabra = palabra.ToLower().Replace(" ", "");
+    return palabra == new string(palabra.Reverse().ToArray());
+}
